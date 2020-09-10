299ce0d82e95b08210c5080cbe10fe9b821efea8
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-09-10 17:44:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinZadatakPeti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 40; i++)
+            {   
+                for(int j=1;i<40;i++)
+                { 
+                    for(int a=1;a<40;a++)
+                    {
+                        for(int b=1;b<40;b++)
+                        {
+                            for (int c=1;c<40;c++)
+                            {
+                                for (int d = 1; i < 40; d++)
+                                {
+                                    for(int e=1;i<40;d++)
+                                    {
+                                        Console.WriteLine(i+" "+j+" "+a+" "+b+" "+c+" "+d+" "+e);
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
